bf5c5a931ee7f515079affaedf5f690746b5535d
Vaibhav Jagannath Desai <desaivaibhav94@gmail.com>
2014-09-08 08:51:40 +0530

    config file for lowes.com

---
+++ source/lowesConfig.py
@@ -0,0 +1,10 @@
+title_css_path = "#descCont > div.itemInfo > h1"
+img_css_path = "#prodPrimaryImg"
+brand_css_path = "#invalid"
+#In lowes.com extracting the price is not possible as it is dynamically loaded
+price_css_path = "#pricing > span"
+#This is the filename the data to be stored in
+filename = "sample.xls"
+site_map_url = "http://www.lowes.com/sitemap.xml"
+site_name = "www.lowes.com"
+db_name = "lowes"
